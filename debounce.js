108de08578a8ea231aaf84c0d15668894275a8df
garry彭 <250631521@qq.com>
2022-02-23 17:47:52 +0800

    debounce

---
+++ debounce.js
@@ -0,0 +1,53 @@
+/**
+ * 
+ * @param {*} fn 需要执行的函数
+ * @param {*} wait 多久开始执行
+ * @param {*} immediate true： 执行第一次； false：执行最后一次
+ * @returns 
+ */
+const debounce = (fn, wait, immediate) => {
+    if (typeof fn !== 'function') {
+        throw new Error('fn must be an function')
+    }
+
+    if (typeof wait === 'undefined') {
+        wait = 300
+    }
+
+    if (typeof wait === 'boolean') {
+        immediate = wait
+        wait = 300
+    }
+
+    if (typeof immediate !== 'boolean') {
+        immediate = false
+    }
+
+    let timer
+
+    return function proxy(...args) {
+        let self = this
+        let init = immediate && !timer
+
+        clearTimeout(timer)
+
+        timer = setTimeout(() => {
+            clearTimeout(timer)
+
+            !immediate && fn.call(self, ...args)
+        }, wait)
+
+        init && fn.call(self, ...args)
+    }
+}
+
+
+// example
+
+/*
+const fn = debounce((message) => console.log(message), 1000, true)
+
+for (let i = 0; i < 100; i++) {
+    fn(i)
+}
+*/
